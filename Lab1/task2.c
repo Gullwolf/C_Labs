39a1796fbc9a06abdf9341cc432aab6baaedbfcb
George Cook <thegekko07@gmail.com>
2021-12-28 23:23:07 +0000

    Add files via upload

---
+++ Lab1/task2.c
@@ -0,0 +1,13 @@
+//
+// Created by George Cook on 27/01/2021.
+//
+
+#include <stdio.h>
+#include "fibonacci.h"
+#define FIBONACCI_NUM 10
+
+int main(int argc, char* argv[]) {
+    int result = fibonacci(FIBONACCI_NUM);
+    printf("Fibonacci of %d = %d\n", FIBONACCI_NUM, result);
+    return 0;
+}
